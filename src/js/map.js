b58f2d0ec0a338fb3066a37b8b0a56686c7fbb8e
fredrikastjernlof <fredrika.stjernlof@live.se>
2026-03-02 18:09:46 +0100

    Initierar kart-sidan och hämtar DOM-element

--- src/js/map.js
+++ src/js/map.js
@@ -1,10 +1,19 @@
-"use strict"
+"use strict";
 
+// Initierar kart-sidan och hämtar DOM-element
 export function initMap() {
-
     const page = document.querySelector("#map-page");
     if (!page) return;
-    
-    console.log("Hello from map-page!");
 
+    const form = document.querySelector("#map-form");
+    const input = document.querySelector("#place");
+    const status = document.querySelector("#map-status");
+    const frame = document.querySelector("#map-frame");
+    const myBtn = document.querySelector("#my-location");
+
+    console.log("Form:", form);
+    console.log("Input:", input);
+    console.log("Status:", status);
+    console.log("Iframe:", frame);
+    console.log("Min position-knapp:", myBtn);
 }
